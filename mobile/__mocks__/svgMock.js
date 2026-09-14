44da407409cd6eb080e7815022ffda47bdc8fcb8
Enzo Brasca <brascaenzo@gmail.com>
2026-09-14 20:45:23 -0300

    feat(mobile): configurar EAS Update y submit a Google Play
    - app.json: runtimeVersion fingerprint, updates.url, permiso de camara
    - eas.json: channel por profile, submit.production.android (track alpha)
    - versionCode 11 -> 13 tras los intentos de subida a Play Console
    - .gitignore: excluir google-service-account.json

--- mobile/__mocks__/svgMock.js
+++ mobile/__mocks__/svgMock.js
@@ -1,5 +1,3 @@
-// Metro transforma los .svg con react-native-svg-transformer, pero Jest no usa
-// Metro: sin este mock, cualquier test que toque un modulo que importa un icono
-// falla al parsear el XML del SVG como JavaScript.
+
 module.exports = 'SvgMock';
 module.exports.ReactComponent = 'SvgMock';
